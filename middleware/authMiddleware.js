0b0708ec8bd62589120aa6e7387ffc3721a3a3a5
lopingsuee <umaygans03@gmail.com>
2025-06-03 16:48:50 +0700

    feat(prisma): refactoring from array to postgres databases
    Add postgres sql untuk menyimpan data login user dan todos
    
    Closes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,7 +7,7 @@ module.exports = function authenticateToken(req, res, next) {
   if (!token) return res.status(401).json({ message: "Token required" });
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
+    if (err) return res.status(403).json({ message: "Invalid tokeno" });
     req.user = user;
     next();
   });
