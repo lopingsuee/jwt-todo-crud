99ef2066c3bacb25c06551605de862dce0d3a45f
lopingsuee <umaygans03@gmail.com>
2025-06-03 14:01:24 +0700

    saved: update delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -32,3 +32,42 @@ exports.getUserTodos = (req, res) => {
 
   return successResponse(res, 200, "User's todos fetched", userTodos);
 };
+
+exports.updateTodo = (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  const email = req.user.email;
+
+  const todo = todos.find((t) => t.id === parseInt(id));
+
+  if (!todo) {
+    return errorResponse(res, 404, "Todo not found");
+  }
+
+  if (todo.email !== email) {
+    return errorResponse(res, 403, "Unauthorized: Not your todo");
+  }
+
+  if (title) todo.title = title;
+  if (description) todo.description = description;
+
+  return successResponse(res, 200, "Todo updated", todo);
+};
+
+exports.deleteTodo = (req, res) => {
+  const { id } = req.params;
+  const email = req.user.email;
+
+  const index = todos.findIndex((t) => t.id === parseInt(id));
+  if (index === -1) {
+    return errorResponse(res, 404, "Todo not found");
+  }
+
+  if (todos[index].email !== email) {
+    return errorResponse(res, 403, "Unauthorized: Not your todo");
+  }
+
+  const deleted = todos.splice(index, 1)[0];
+
+  return successResponse(res, 200, "Todo deleted", deleted);
+};
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,11 +1,23 @@
 
 const express = require('express');
 const router = express.Router();
-const { createTodo, getUserTodos } = require('../controllers/todoController');    
-const authenticateToken = require('../middleware/authMiddleware');  
+const {
+  createTodo,
+  getUserTodos,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoController");const authenticateToken = require('../middleware/authMiddleware');  
 
-router.post("/todos", authenticateToken, createTodo); // Endpoint untuk membuat todo
-router.get("/todos", authenticateToken, getUserTodos); // Read
+// GET all user todos
+router.get("/", authenticateToken, getUserTodos);
 
+// POST create todo
+router.post("/", authenticateToken, createTodo);
+
+// PUT update todo
+router.put("/:id", authenticateToken, updateTodo);
+
+// DELETE todo
+router.delete("/:id", authenticateToken, deleteTodo);
 
 module.exports = router;
